27b6c1cf284c4b81c333a96f97437ce7828b53a9
Jahle <Jahle@ABLACKMAN>
2024-05-31 15:20:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ElementFrequencyCounter
+{
+    class Program
+    {
+        static Dictionary<int, int> _selectionCount = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            int[] elementsArray = { 10, 12, 13, 12, 10 };
+
+            foreach (int e in elementsArray)
+            {
+                AddElements(e);
+            }
+
+            foreach (KeyValuePair<int, int> pair in _selectionCount)
+            {
+                Console.WriteLine(format: "{0} occurs {1} times", pair.Key, pair.Value);
+            }
+
+        }
+
+        static public void AddElement(int element)
+        {
+            if (_selectionCount.ContainsKey(element))
+            {
+                _selectionCount[element]++;
+            }
+            else
+            {
+                _selectionCount.Add(element, 1);
+            }
+        }
+    }
+}
+        
+                    
